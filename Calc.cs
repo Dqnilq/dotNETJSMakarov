aad810f47d13c7d3af004a485da1797f50792248
Danila Makarov <makarov01V@ya.ru>
2020-09-07 20:11:57 +0300

    ccc

--- Calc.cs
+++ Calc.cs
@@ -6,15 +6,57 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-   class Calculator
+    public class Calculator
     {
+        public static int NewMethod(int a, string oper, int b)
+        {
+            return oper switch
+            {
+                "+" => a + b,
+                "-" => a - b,
+                "*" => a * b,
+                "/" => a / b,
+                => throw new NotSupportedException()
+            };
+        }
 
+        public static int GetNumber()
+        {
+            return int.Parse(Console.ReadLine());
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var a = Calculator.GetNumber();
+            var oper = (Console.ReadLine());
+            var b = Calculator.GetNumber();
+            var res = Calculator.NewMethod(a, oper, b);
+            Console.WriteLine(res);
+
+            switch (oper)
+            {
+                case "+":
+                    Console.WriteLine(a + b);
+                    break;
+                case "-":
+                    Console.WriteLine(a - b);
+                    break;
+                case "*":
+                    Console.WriteLine(a * b);
+                    break;
+                case "/":
+                    Console.WriteLine(a / b);
+                    break;
+            }
         }
+
+
+
+
+
+
+        
     }
 }
