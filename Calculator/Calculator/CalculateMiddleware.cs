371a6b0e70ca3e212875aa3e824de7cbc46c36a5
Danila Makarov <makarov01V@ya.ru>
2020-10-18 15:27:58 +0300

    ASP.Net Calculator Sever+FSharp proxy added

---
+++ Calculator/Calculator/CalculateMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using CLibraryC;
+using Microsoft.AspNetCore.Http;
+
+namespace Calculator
+{
+    public class CalculateMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string variable_name;
+
+        public CalculateMiddleware(RequestDelegate next, string variableName)
+        {
+            _next = next;
+            variable_name = variableName;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                var a = CalculatorC.Calculate(context.Request.Query[variable_name]);
+                _next.Invoke(context);
+
+            }
+            catch
+            {
+                context.Response.StatusCode = 400;
+            }
+        }
+
+    }
+}
---
+++ Calculator/Calculator/TokenExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Calculator
+{
+    public static class TokenExtensions
+    {
+        public static IApplicationBuilder UseExpression(this IApplicationBuilder builder, string pattern)
+        {
+            return builder.UseMiddleware<ExpressionMiddleware>(pattern); 
+        }
+        
+        public static IApplicationBuilder UseCalculator(this IApplicationBuilder builder, string pattern)
+        {
+            return builder.UseMiddleware<CalculateMiddleware>(pattern); 
+        }
+    }
+}
