8fddc68c7ee8f15c4390f6f6685d082db8c54085
Danila Makarov <makarov01V@ya.ru>
2020-10-27 16:54:41 +0300

    not ended tree calc end etc

---
+++ ClientExpressions/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+
+namespace ClientExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
